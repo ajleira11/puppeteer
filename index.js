d9f0795674be843517c146997a850b30033520a6
Ariel Joseph Amano <arieljosephamano@gmailcom>
2024-02-29 13:02:10 +1300

    updated local scraper for lever

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const puppeteer = require("puppeteer");
 async function run() {
   try {
     const leverURL = "https://www.lever.co/";
-    const inputURL = "https://jobs.lever.co/safetyculture-2/";
+    const inputURL = "https://jobs.lever.co/health-match/";
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     console.log("Launching browser...");
@@ -57,8 +57,8 @@ async function run() {
             areas: areas,
             title: titleElement ? titleElement.innerText : "",
             location: locationElement ? locationElement.innerText : "",
-            workStyle: workStyleElement ? workStyleElement.innerText : "",
-            workType: workType,
+            workStyle: workStyleElement ? [workStyleElement.innerText] : [""],
+            workType: [workType],
             jobURL: jobURLElement ? jobURLElement.href : "",
           };
         }),
@@ -67,8 +67,9 @@ async function run() {
 
     const updatedJobs = await addDescription(jobs, browser);
 
+    const output = await formatOutput(updatedJobs);
     //open job links and add the description inside the jobs list
-    console.log("Jobs:", updatedJobs, updatedJobs.length);
+    console.log("Jobs:", output);
 
     await browser.close();
   } catch (error) {
@@ -99,9 +100,9 @@ async function addDescription(jobs, browser) {
       workStyle: job.workStyle,
       workType: job.workType,
       seniority: "",
-      location: job.location,
+      location: [job.location],
       timing: "",
-      areas: job.areas,
+      areas: [job.areas],
       images: "",
       video: "",
       audio: "",
@@ -118,6 +119,85 @@ async function addDescription(jobs, browser) {
   } catch (error) {
     console.error("An error occurred:", error);
   }
+}
+
+async function formatOutput(jobs) {
+  for (let job of jobs) {
+    job.workStyle = job.workStyle.flatMap((style) => {
+      if (!style) {
+        return [""];
+      }
+      //convert style to single array each word,
+      const separateStyleArray = style
+        .toString()
+        .split(/\s+|(?<!\w)(?=\W)|(?<=\W)(?!\w)/);
+
+      const finalStyle = separateStyleArray
+        .map((style) => {
+          const formatStyle =
+            style.charAt(0).toUpperCase() + style.slice(1).toLowerCase();
+          return formatStyle;
+        })
+        .filter((style) => style.length > 1); //filter out each element in an array where
+
+      return finalStyle;
+    });
+
+    //----------------------------------------------------------------
+
+    job.workType = await job.workType.flatMap((type) => {
+      if (!type) {
+        return [""];
+      }
+      const separateTypeArray = type.toString().split(" ");
+
+      const finalType = separateTypeArray
+        .map((type) => {
+          const formatType =
+            type.charAt(0).toUpperCase() + type.slice(1).toLowerCase();
+          if (formatType === "Full") {
+            return "Full-time";
+          } else if (formatType === "Part") {
+            return "Part-time";
+          } else {
+            return formatType;
+          }
+        })
+        .filter(
+          (type) => type.length > 1 && type !== "Time" && type !== "Term"
+        ); // Filter out elements with length <= 1 and also remove type
+
+      return finalType;
+    });
+
+    job.location = await capitalizeWords(job.location);
+    job.areas = await capitalizeWords(job.areas);
+  }
+
+  return jobs;
+}
+
+async function capitalizeWords(str) {
+  // if (str.length <= 1) {
+  //   return [""];
+  // }
+  // Convert the string to lowercase and split it into words
+  const words = str[0].toLowerCase();
+  if (words.length <= 1) {
+    return [""];
+  }
+
+  const newWords = words.split(" ");
+
+  // Capitalize the first letter of each word
+  const capitalizedWords = newWords.map((word) => {
+    // Capitalize the first letter of the word and concatenate it with the rest of the word
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  });
+
+  // Join the capitalized words back into a single string
+  const toArray = capitalizedWords.join(" ");
+  return [toArray];
 }
 
 run();
